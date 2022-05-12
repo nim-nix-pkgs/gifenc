df1b528eb1ebd17cdfb122acd2e2089e2fd13ca0
hugosenari <hugosenari@gmail.com>
2022-05-12 11:51:56 -0300

    chore: re index gifenc

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."gifenc-master".dir   = "master";
+  inputs."gifenc-master".owner = "nim-nix-pkgs";
+  inputs."gifenc-master".ref   = "master";
+  inputs."gifenc-master".repo  = "gifenc";
+  inputs."gifenc-master".type  = "github";
+  inputs."gifenc-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."gifenc-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
